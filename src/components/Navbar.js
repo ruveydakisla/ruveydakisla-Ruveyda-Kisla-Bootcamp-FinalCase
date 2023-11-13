b9a3a4b86fb9876e2462e32ea14edeac83f17e2f
ruveydakisla <ruveydakisla34@gmail.com>
2023-11-13 21:49:15 +0300

    frontend decoration

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link, Router,Route } from "react-router-dom";
 
 const Navbar=()=>{
     return(
         <div>
-
+            <Router>
+                <nav className="nav">
+                    <ul>
+                        <Link to='/SignIn'>
+                            <li>
+                                SignIn
+                            </li>
+                        </Link>
+                        <Link to='/SignUp'>
+                            <li>
+                                SignUp
+                            </li>
+                        </Link>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path='/SignIn' element={<SignIn/>}>
+                    
+                </Routes>
+            </Router>
         </div>
     )
 }
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,9 +1,12 @@
 import React from "react";
+import FormField from "../components/FormField";
 
 const SignIn=()=>{
     return(
         <div>
             <h1>Sign In</h1>
+            <FormField name={address}/>
+
         </div>
     )
 }
